79e19c53236be55c3e39db784cfa2d3ac15f0d9d
Fakhreddin <fakhredin0098@gmail.com>
2022-05-30 02:59:12 -0400

    simulation up and runnign

---
+++ devel/share/turtlebot3_msgs/cmake/turtlebot3_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(turtlebot3_msgs_MESSAGE_FILES "/home/thefakhreddin/catkin_ws/src/turtlebot3_msgs/msg/SensorState.msg;/home/thefakhreddin/catkin_ws/src/turtlebot3_msgs/msg/VersionInfo.msg;/home/thefakhreddin/catkin_ws/src/turtlebot3_msgs/msg/Sound.msg")
+set(turtlebot3_msgs_SERVICE_FILES "")
